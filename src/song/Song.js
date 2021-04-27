19cfc134df2de37af517a6b12fd29568b9deff06
percworld <percworld@gmail.com>
2021-04-26 22:29:36 -0600

    Add getSongDetails fetch to utilities

--- src/song/Song.js
+++ src/song/Song.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { getSong } from './../utilities';
+import { getSong, getPlays } from './../utilities';
 
 const Song = ({ songID }) => {
-    const songDetails = getSong(songID);
-    console.log("SongID: ", songID)
-    return (
 
-        <p>{songID}</p>
+    const songDetails = getSong(songID);
+    console.log(songDetails);
+    const playDetails = getPlays(songID);
+    console.log(playDetails);
+    return (
+        <article>
+            <p>{songDetails.name}</p>
+            {/* <p>{playDetails}</p> */}
+        </article>
     )
+
+    // try {
+    //     const songDetails = await getSong(songID);
+    //     console.log(songDetails);
+    //     const playDetails = await getPlays(songID);
+    //     console.log(playDetails);
+    //     return (
+    //         <article>
+    //             {/* <p>{songDetails.name}</p> */}
+    //             {/* <p>{playDetails}</p> */}
+    //         </article>
+    //     )
+    // } catch (error) {
+    //     console.log(error)
+    // }
 
 }
 

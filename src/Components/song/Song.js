3ecabd7c80eea5dbf7a3907ba57a8165495f2e6d
percworld <percworld@gmail.com>
2021-04-30 00:09:20 -0600

    Add state to Song Component

--- src/Components/song/Song.js
+++ src/Components/song/Song.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getSong, getPlays } from '../../api';
 
 const Song = ({ songID }) => {
+    const [feature, setFeature] = useState();
+    const details = async () => {
+        try {
+            const songDetails = await getSong(songID);
+            console.log(songDetails);
+            //setFeature(songDetails);
+            const playDetails = await getPlays(songID);
+            console.log(playDetails);
+        } catch {
+            throw new Error('Whyyyy')
+        }
+    }
 
-    const songDetails = getSong(songID);
-    console.log(songDetails);
-    const playDetails = getPlays(songID);
-    console.log(playDetails);
     return (
         <article>
-            <p>{songDetails.name}</p>
-            {/* <p>{playDetails}</p> */}
+            <p>details</p>
+            <p>{feature}</p>
         </article>
     )
-
     // try {
     //     const songDetails = await getSong(songID);
     //     console.log(songDetails);

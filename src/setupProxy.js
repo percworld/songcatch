2a282222d41c73dfc8464d9a0c3d4e0c3fbe3875
percworld <percworld@gmail.com>
2021-05-07 16:50:07 -0600

    Fix proxy middleare to correct port and include dependency

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://www.phantasytour.com/api',
+      target: 'http://localhost:3000',
       changeOrigin: true,
     })
   );

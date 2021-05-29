df3c9704ad216edb4ec507820ceb897a54cd4008
percworld <percworld@gmail.com>
2021-05-29 17:03:31 -0600

    Switch to install cors

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
@@ -9,21 +10,27 @@ const checkResponse = (response) => {
     if (!response.ok) {
         throw new Error('The songs aren\'t currently available.');
     } else {
+        console.log(response)
         return response.json();
     }
 }
 
+// const logger = (req, res, next) => {
+//     console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
+//     next();
+// }
+// app.use(logger);
 
 app.get('/', (req, res) => {
     res.send('<h1>Setlift</h1>')
 })
 
 app.get('/songs/', async (req, res) => {
-    //get id 
-    const bandId = req.query.Id;
+    console.log(req.query.bandId)
+    const bandId = req.query.bandId;
     const songs = await fetch(`https://phantasytour.com/api/songs/?bandId=${bandId}`)
         .then(checkResponse)
-    console.log(songs)
+    console.log(songs[100])
     res.json(songs);
 })
 
@@ -36,6 +43,13 @@ app.get('/songs/', async (req, res) => {
 //     res.json(song);
 
 // }
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next()
+})
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`${app.locals.title} Server started on ${PORT}`))

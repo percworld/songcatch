cbe9e9f4b5110632fd93c165781362db7db61148
percworld <percworld@gmail.com>
2021-05-31 22:34:34 -0600

    Fix path to public

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 app.use(cors())
 app.locals.title = "Setlift";

eceaeab712d0aad430c50e8e003729f8b3c1cf50
percworld <percworld@gmail.com>
2021-07-21 10:56:32 -0600

    Add star svg

--- src/Components/header/Header.js
+++ src/Components/header/Header.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import './Header.scss';
-//import {  } from '../../assets/celtic logo thick Vector.ai'
+import { ReactComponent as Star  } from '../icons/star.svg'
 
 const Header = () => {
     return (
         <header>
             <Link to='/' className='homeLink'>
-                {/* <img src={'../../assets/celtic logo thick Vector.ai'} /> */}
-                {/* <img src={`${process.env.PUBLIC_URL}../../public/assets/lotuslogo-removebg-preview.png`} alt="lotus logo" /> */}
-
                 <h3 className='title' data-cy='setlift'>Setlist</h3>
+                {/* <Star className='star'></Star> */}
             </Link>
 
 
